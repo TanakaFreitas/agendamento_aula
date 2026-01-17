d1517aa9a343ef0db9ef654b2379f09862cbc11d
TanakaFreitas <tanakafreitas@gmail.com>
2026-01-16 23:19:21 -0300

    add page callback confirm.vue

---
+++ app/pages/confirm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useSupabaseUser, useSupabaseCookieRedirect } from '#imports'
+import { navigateTo } from '#app'
+
+const user = useSupabaseUser()
+const redirectInfo = useSupabaseCookieRedirect()
+
+watch(user, () => {
+  if (user.value) {
+    const path = redirectInfo.pluck()
+    return navigateTo(path || '/')
+  }
+}, { immediate: true })
+</script>
+
+<template>
+  <div id="confirm-page" class="flex items-center justify-center min-h-screen">
+    <div class="text-center">
+      <p>Aguardando confirmação do login...</p>
+    </div>
+  </div>
+</template>
